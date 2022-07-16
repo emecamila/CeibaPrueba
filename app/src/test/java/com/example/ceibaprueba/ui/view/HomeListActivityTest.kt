490dc42eaa12ef4d5ec4fd38bf46b5871b74a853
Camila Arbelaez <arbelaez.mag@gmail.com>
2022-07-15 19:17:37 -0500

    add Espresso Test Class For Home

---
+++ app/src/test/java/com/example/ceibaprueba/ui/view/HomeListActivityTest.kt
@@ -0,0 +1,19 @@
+package com.example.ceibaprueba.ui.view
+
+
+import org.junit.jupiter.api.Test
+
+internal class HomeListActivityTest {
+
+    @Test
+    fun onCreate() {
+    }
+
+    @Test
+    fun onQueryTextChange() {
+    }
+
+    @Test
+    fun onUserRecyclerViewCLickListener() {
+    }
+}
